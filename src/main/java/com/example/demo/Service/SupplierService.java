5f40286b2bc029be5a0984ee2f2d78891743751b
Walaa Ali <105892469+Walaaali782@users.noreply.github.com>
2025-08-07 21:11:34 +0300

    Add files via upload

---
+++ src/main/java/com/example/demo/Service/SupplierService.java
@@ -0,0 +1,31 @@
+package com.example.demo.Service;
+
+import com.example.demo.Entity.*;
+import com.example.demo.Repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SupplierService {
+    @Autowired
+    private SupplierRepository supplierRepository;
+
+    public List<Supplier> getAllSuppliers() {
+        return supplierRepository.findAll();
+    }
+
+    public Optional<Supplier> getSupplierById(Long id) {
+        return supplierRepository.findById(id);
+    }
+
+    public Supplier saveSupplier(Supplier supplier) {
+        return supplierRepository.save(supplier);
+    }
+
+    public void deleteSupplier(Long id) {
+        supplierRepository.deleteById(id);
+    }
+}
